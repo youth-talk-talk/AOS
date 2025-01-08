195810329b04ab901fa7f331b43701b5bf076ef5
윤찬 <vsvx13@naver.com>
2025-01-08 22:42:03 +0900

    [refactor] api url 수정

--- core/data/src/main/java/com/youthtalk/data/AnnounceService.kt
+++ core/data/src/main/java/com/youthtalk/data/AnnounceService.kt
@@ -9,9 +9,14 @@ import retrofit2.http.Query
 
 interface AnnounceService {
 
-    @GET("/announcements")
+    /**
+     * 회원 > 회원 탈퇴
+     * @see <a href="https://web.postman.co/workspace/My-Workspace~3c068cce-5f49-44cd-8661-9bdd02a8cf87/request/25820857-12e73469-506e-4539-9a3a-5b199dd1a548?action=share&source=copy-link&creator=25820857">PostMan 링크</a>
+     * @return [Unit]
+     * */
+    @GET("/api/v1/announcements")
     suspend fun getAllAnnounce(@Query("size") size: Int, @Query("page") page: Int): CommonResponse<AnnouncesResponse>
 
-    @GET("/announcements/{id}")
+    @GET("/api/v1/announcements/{id}")
     suspend fun getAnnounceDetail(@Path("id") id: Long): CommonResponse<AnnounceDetailResponse>
 }
--- core/data/src/main/java/com/youthtalk/data/CommentService.kt
+++ core/data/src/main/java/com/youthtalk/data/CommentService.kt
@@ -12,21 +12,21 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface CommentService {
-    @GET("/policies/{policyId}/comments")
+    @GET("/api/v1/policies/{policyId}/comments")
     suspend fun getPolicyComment(@Path("policyId") policyId: String): CommonResponse<List<CommentResponse>>
 
-    @DELETE("/comments/{commentId}")
+    @DELETE("/api/v1/comments/{commentId}")
     suspend fun postDeleteComment(@Path("commentId") commentId: Long): CommonResponse<PostAddCommentResponse>
 
-    @GET("/members/me/comments")
+    @GET("/api/v1/members/me/comments")
     suspend fun getMyComments()
 
-    @GET("/posts/{postId}/comments")
+    @GET("/api/v1/posts/{postId}/comments")
     suspend fun getPostDetailComments(@Path("postId") postId: Long): CommonResponse<List<CommentResponse>>
 
-    @POST("/comments/likes")
+    @POST("/api/v1/comments/likes")
     suspend fun postLikes(@Body requestBody: RequestBody): CommonResponse<Unit>
 
-    @PATCH("/comments")
+    @PATCH("/api/v1/comments")
     suspend fun patchComment(@Body requestBody: RequestBody): CommonResponse<Unit>
 }
