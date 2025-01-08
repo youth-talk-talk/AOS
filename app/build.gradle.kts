9ffd980f3ccec5ded37a317838ea94f4793848dc
윤찬 <vsvx13@naver.com>
2025-01-08 22:59:11 +0900

    [version] 1.0.12 -> 1.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.youth.yongproject.app"
         targetSdk = 34
-        versionCode = 1012
-        versionName = "1.0.12"
+        versionCode = 1013
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
