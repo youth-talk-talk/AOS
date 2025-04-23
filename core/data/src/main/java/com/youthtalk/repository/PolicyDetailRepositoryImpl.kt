5b8e53b1dac2d9a23cdd293f2fd00381e0c51a20
윤찬 <vsvx13@naver.com>
2025-04-23 12:41:08 +0900

    [update]: 버전 업데이트

--- core/data/src/main/java/com/youthtalk/repository/PolicyDetailRepositoryImpl.kt
+++ core/data/src/main/java/com/youthtalk/repository/PolicyDetailRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.youthtalk.repository
 
+import android.util.Log
 import com.core.dataapi.repository.PolicyDetailRepository
 import com.core.exception.NoDataException
 import com.youthtalk.data.PolicyService
@@ -9,6 +10,7 @@ import com.youthtalk.model.PolicyDetail
 import com.youthtalk.utils.ErrorUtils.throwableError
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 import javax.inject.Inject
 
 class PolicyDetailRepositoryImpl @Inject constructor(
@@ -25,6 +27,8 @@ class PolicyDetailRepositoryImpl @Inject constructor(
                 } ?: throw NoDataException("no Data")
             }
             .onFailure {
+                Log.e("YOON-CHAN", "getPolicyDetail $it")
+                Timber.e("getPolicyDetail $it")
                 throwableError<PolicyDetailResponse>(it)
             }
     }
