50a171bfac9d2fb7c852482395936dd3c9fb64f3
윤찬 <vsvx13@naver.com>
2025-04-29 17:32:00 +0900

    [docs]: 기능 수정 초기화 및 버전 2.0.0 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.youth.yongproject.app"
         targetSdk = 35
-        versionCode = 10101
-        versionName = "1.1.1"
+        versionCode = 20000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
