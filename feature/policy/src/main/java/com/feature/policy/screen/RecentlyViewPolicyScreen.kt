3b481657d6f266329b0200d3bd088c7fc30d285a
윤찬 <vsvx13@naver.com>
2025-05-02 20:07:49 +0900

    [feat]: 비어있는 경우 화면 UI 구현

--- feature/policy/src/main/java/com/feature/policy/screen/RecentlyViewPolicyScreen.kt
+++ feature/policy/src/main/java/com/feature/policy/screen/RecentlyViewPolicyScreen.kt
@@ -2,9 +2,12 @@ package com.feature.policy.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,9 +15,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.youthtalk.component.card.PolicyCard
+import com.youthtalk.component.empty.EmptyScreen
 import com.youthtalk.component.topbar.MiddleTitleTopBar
 import com.youthtalk.designsystem.gray10
 import com.youthtalk.designsystem.gray40
@@ -22,6 +30,9 @@ import com.youthtalk.designsystem.gray80
 
 @Composable
 fun RecentlyViewPolicyScreen(modifier: Modifier = Modifier) {
+    var count by remember {
+        mutableStateOf(5)
+    }
     Column(
         modifier = modifier
             .fillMaxSize(),
@@ -39,28 +50,48 @@ fun RecentlyViewPolicyScreen(modifier: Modifier = Modifier) {
             },
         )
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 10.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            items(
-                count = 20,
+        if (count == 0) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(2f),
+            )
+            EmptyScreen(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(5f),
+                emptyTitle = "최근 본 정책이 없어요",
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 10.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
-                PolicyCard(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(
-                            color = gray10,
-                            shape = RoundedCornerShape(12.dp),
-                        )
-                        .border(
-                            width = 1.dp,
-                            color = gray40,
-                            shape = RoundedCornerShape(12.dp),
-                        ),
-                )
+                items(
+                    count = count,
+                ) {
+                    PolicyCard(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(
+                                color = gray10,
+                                shape = RoundedCornerShape(12.dp),
+                            )
+                            .border(
+                                width = 1.dp,
+                                color = gray40,
+                                shape = RoundedCornerShape(12.dp),
+                            )
+                            .clickable(
+                                indication = null,
+                                interactionSource = remember { MutableInteractionSource() },
+                            ) {
+                                count--
+                            },
+                    )
+                }
             }
         }
     }
