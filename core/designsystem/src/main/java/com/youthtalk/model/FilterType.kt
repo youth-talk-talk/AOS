89b5ff8e20a4d57502a0c3de010e955bb650ec17
이건희 <leech9876@naver.com>
2025-06-09 17:29:44 +0900

    #228 - refactor : FilterType의 title을 만들어서 중복으로 title을 찾는 코드 삭제

--- core/designsystem/src/main/java/com/youthtalk/model/FilterType.kt
+++ core/designsystem/src/main/java/com/youthtalk/model/FilterType.kt
@@ -1,10 +1,10 @@
 package com.youthtalk.model
 
-enum class FilterType {
-    POLICY_TYPE,
-    REGION,
-    RECRUIT,
-    EDUCATION,
-    SPECIALIZED,
-    AGE_EARN
+enum class FilterType(val title: String) {
+    POLICY_TYPE("정책분야"),
+    REGION("지역"),
+    RECRUIT("취업상태"),
+    EDUCATION("학력"),
+    SPECIALIZED("특화 분야"),
+    AGE_EARN("연령 및 소득")
 }
