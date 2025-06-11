6958fdee2a132932225fb618e9440061a9d4d728
이건희 <leech9876@naver.com>
2025-06-11 21:55:16 +0900

    #238 - feat : 알람 화면에 뒤로가기 버튼 활성화

--- feature/main/src/main/java/com/youthtalk/MainScreen.kt
+++ feature/main/src/main/java/com/youthtalk/MainScreen.kt
@@ -118,7 +118,9 @@ fun NavHostScreen(
             onClickPostDetail = navController::navigateCommunityDetail,
             onClickPolicyDetail = navController::navigatePolicyDetail
         )
-        settingNotificationNavigation()
+        settingNotificationNavigation(
+            onBack = navController::popBackStack
+        )
 
         policySearchNavigation(
             onBack = { navController.popBackStack() },
