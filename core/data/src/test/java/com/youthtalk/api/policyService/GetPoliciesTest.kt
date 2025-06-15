c8602cc22ee1a47d846337f3c783e1dc6afcc0c2
이건희 <leech9876@naver.com>
2025-06-15 14:27:37 +0900

    #246 - test : 정책들을 불러오는 테스트 코드 작성

--- core/data/src/test/java/com/youthtalk/api/policyService/GetPoliciesTest.kt
+++ core/data/src/test/java/com/youthtalk/api/policyService/GetPoliciesTest.kt
@@ -5,6 +5,8 @@ import com.youthtalk.api.ApiTestUtils.createRetrofit
 import com.youthtalk.api.interceptor.TestAuthInterceptor
 import com.youthtalk.api.policyService.json.homePolicyJson
 import com.youthtalk.api.policyService.json.newPolicyJson
+import com.youthtalk.api.policyService.json.recentlyViewPoliciesJson
+import com.youthtalk.api.policyService.json.scrapedPoliciesJson
 import com.youthtalk.data.PolicyService
 import com.youthtalk.model.policy.Policy
 import kotlinx.coroutines.runBlocking
@@ -14,6 +16,7 @@ import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Test
@@ -99,5 +102,58 @@ class GetPoliciesTest {
             emptyList<Policy>(),
             policies.all + policies.job + policies.life + policies.dwelling + policies.education + policies.participation
         )
+    }
+
+    @Test
+    fun given_whenGetScrapedPolicies_thenReturnsScrapedPolicies() = runBlocking {
+        // given
+        val responseJson = scrapedPoliciesJson
+
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(responseJson)
+        )
+
+        // when
+        val response = sut.getScrapPolicies(page = 0, size = 10)
+        val recordedRequest = mockWebServer.takeRequest()
+
+        // then
+        assertEquals("/api/v1/policies/scrap?page=0&size=10", recordedRequest.path)
+
+        // 응답 body 검증
+        assertEquals(200, response.status)
+        assertEquals("요청에 성공하였습니다.", response.message)
+        assertEquals("S01", response.code)
+
+        val policies = response.data ?: return@runBlocking fail("정책들은 null 값이 되면 안됨")
+        assertTrue(policies.all { it.scrap })
+    }
+
+    @Test
+    fun given_whenGetRecentViewPolicies_thenReturnsRecentViewPolicies() = runBlocking {
+        // given
+        val responseJson = recentlyViewPoliciesJson
+
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(responseJson)
+        )
+
+        // when
+        val response = sut.getRecentlyViewPolicies()
+        val recordedRequest = mockWebServer.takeRequest()
+
+        // then
+        assertEquals("/api/v1/policies/recent-view", recordedRequest.path)
+
+        // 응답 body 검증
+        assertEquals(200, response.status)
+        assertEquals("요청에 성공하였습니다.", response.message)
+        assertEquals("S01", response.code)
+
+        assertNotNull(response.data)
     }
 }
