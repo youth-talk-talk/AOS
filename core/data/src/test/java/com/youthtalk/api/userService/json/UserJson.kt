990d3a532affada70febf5104f306b2b9060220c
이건희 <leech9876@naver.com>
2025-06-16 21:41:20 +0900

    #248 - test : 나의 정보들을 불러오는 테스트코드 작성
    myComments와 likeComments의 경우
    UserService로 분류되어있다.
    
    고민 후에 CommentService로 분리하든 유지하든 결정하자

---
+++ core/data/src/test/java/com/youthtalk/api/userService/json/UserJson.kt
@@ -0,0 +1,55 @@
+package com.youthtalk.api.userService.json
+
+val userInfoJson = """
+    {
+      "status": 200,
+      "message": "요청에 성공하였습니다.",
+      "code": "S01",
+      "data": {
+        "memberId": 21,
+        "nickname": "user222",
+        "profileImgUrl": null,
+        "region": "서울"
+      }
+    }
+""".trimIndent()
+
+val myCommentInfo = """
+    {
+      "status": 200,
+      "message": "요청에 성공하였습니다.",
+      "code": "S01",
+      "data": {
+        "commentCount": 2,
+        "comments": [
+          {
+            "commentId": 91,
+            "content": "리뷰댓글222",
+            "articleId": 35,
+            "articleType": "review",
+            "articleTitle": "후기게시글",
+            "isLikedByMember": false,
+            "likeCount": 0
+          },
+          {
+            "commentId": 90,
+            "content": "리뷰댓글111",
+            "articleId": 35,
+            "articleType": "review",
+            "articleTitle": "후기게시글",
+            "isLikedByMember": false,
+            "likeCount": 0
+          }
+        ]
+      }
+    }
+""".trimIndent()
+
+val myCommentInfoNull = """
+    {
+      "status": 200,
+      "message": "해당하는 댓글이 없습니다.",
+      "code": "S09",
+      "data": null
+    }
+""".trimIndent()
