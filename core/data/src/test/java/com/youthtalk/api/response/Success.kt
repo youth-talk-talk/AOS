d01f5c267ed51986828f45a25ddc1cb7fe3bc13b
이건희 <leech9876@naver.com>
2025-06-18 17:33:09 +0900

    #254 - feat : 공통적으로 사용하는 json을 파일로 분리

---
+++ core/data/src/test/java/com/youthtalk/api/response/Success.kt
@@ -0,0 +1,10 @@
+package com.youthtalk.api.response
+
+val commonSuccessJson = """
+    {
+      "status": 200,
+      "message": "요청에 성공하였습니다.",
+      "code": "S01",
+      "data": null
+    }
+""".trimIndent()
