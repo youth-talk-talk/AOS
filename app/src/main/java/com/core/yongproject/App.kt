b72e2b36a5f15389418792156bf0b694b507d566
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-07-02 14:21:19 +0900

    Timber에 자동으로 클래스명이 나오도록 설정 (#291)
    * #290 - feat : 클래스이름, 라인, 메서드를 출력하는 Timber.DebugTree 작성
    
    * #290 - feat : 기존에 앱에서 사용하는 디폴트 디버그 트리가 아닌 커스텀 디버그 트리 사용
    
    * #290 - style : 한줄로 표현 가능한 함수를 중괄호 대신 = 사용

--- app/src/main/java/com/core/yongproject/App.kt
+++ app/src/main/java/com/core/yongproject/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import androidx.work.WorkManager
+import com.core.yongproject.timber.CustomTimberDebugTree
 import com.kakao.sdk.common.KakaoSdk
 import com.youth.app.yongproject.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
@@ -23,7 +24,7 @@ class App : Application(), Configuration.Provider {
         KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
         WorkManager.initialize(this, workManagerConfiguration)
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(CustomTimberDebugTree())
         }
     }
 
