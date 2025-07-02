607eaa7d93ed2637360487bdc9d6c657496998c8
이건희 <leech9876@naver.com>
2025-07-02 13:18:28 +0900

    #287 - feat : 사진 추가하는 ViewModel, UiEvent 삭제
    사진 삭제 관련 코드는 남겨둠

--- feature/community/src/main/java/com/core/community/model/write/CommunityWriteUiEvent.kt
+++ feature/community/src/main/java/com/core/community/model/write/CommunityWriteUiEvent.kt
@@ -4,13 +4,11 @@ import androidx.compose.ui.text.input.TextFieldValue
 import com.core.base.model.UiEvent
 import com.youthtalk.model.policy.SearchPolicy
 import com.youthtalk.model.post.PostSubject
-import java.io.File
 
 sealed interface CommunityWriteUiEvent : UiEvent {
     data class InitData(val postId: Long?, val postSubject: PostSubject) : CommunityWriteUiEvent
     data class OnTextChangeValue(val index: Int, val text: TextFieldValue) : CommunityWriteUiEvent
     data object GetImages : CommunityWriteUiEvent
-    data class PostUploadImages(val file: File) : CommunityWriteUiEvent
     data class FocusChange(val index: Int, val text: TextFieldValue?) : CommunityWriteUiEvent
     data class ChangeTitle(val title: String) : CommunityWriteUiEvent
     data class SearchPolicyChangeTextValue(val searchPolicy: String) : CommunityWriteUiEvent
