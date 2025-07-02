0d34595c15f6aaebd10a4ab0d37362059d8d0e02
이건희 <leech9876@naver.com>
2025-07-02 12:03:25 +0900

    #286 - feat : UserService에서 프로필 이미지 관련 코드 삭제, 해당하는 테스트코드도 삭제

--- core/data/src/test/java/com/youthtalk/api/userService/UpdateUserImageTest.kt
+++ core/data/src/test/java/com/youthtalk/api/userService/UpdateUserImageTest.kt
@@ -4,26 +4,15 @@ import com.youthtalk.api.ApiTestUtils
 import com.youthtalk.api.ApiTestUtils.createRetrofit
 import com.youthtalk.api.interceptor.TestAuthInterceptor
 import com.youthtalk.api.response.commonSuccessJson
-import com.youthtalk.api.userService.json.imageJson
-import com.youthtalk.api.userService.json.sizeOverImageJson
 import com.youthtalk.data.UserService
-import com.youthtalk.dto.CommonResponse
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
-import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
-import org.junit.Assert.assertThrows
 import org.junit.Before
-import org.junit.Test
-import retrofit2.HttpException
 
 class UpdateUserImageTest {
 
@@ -51,72 +40,6 @@ class UpdateUserImageTest {
     @After
     fun tearDown() {
         mockWebServer.shutdown()
-    }
-
-    @Test
-    fun givenImage_whenPost_thenWorksFine() = runBlocking {
-        // given
-        val image = MultipartBody.Part.create(
-            """
-            'image=@"/Users/seulgi/Downloads/test_img.jpeg"'
-            """.trimIndent().toRequestBody("application/json".toMediaType())
-        )
-        val responseJson = imageJson
-
-        mockWebServer.enqueue(
-            MockResponse()
-                .setResponseCode(200)
-                .setBody(responseJson)
-        )
-
-        // when
-        val response = sut.postUserImage(image)
-        val recordedRequest = mockWebServer.takeRequest()
-
-        // then
-        assertEquals("/api/v1/members/profile", recordedRequest.path)
-
-        // 응답
-        assertEquals(200, response.status)
-        assertEquals("요청에 성공하였습니다.", response.message)
-        assertEquals("S01", response.code)
-
-        assertNotNull(response.data)
-    }
-
-    @Test
-    fun givenSizeOverImage_whenPost_thenThrows400Exception() = runBlocking {
-        // given
-        val image = MultipartBody.Part.create(
-            """
-            'image=@"/Users/seulgi/Downloads/over_5MB.jpg"'
-            """.trimIndent().toRequestBody("application/json".toMediaType())
-        )
-        val responseJson = sizeOverImageJson
-
-        mockWebServer.enqueue(
-            MockResponse()
-                .setResponseCode(400)
-                .setBody(responseJson)
-        )
-
-        // when
-        val exception = assertThrows(HttpException::class.java) {
-            runBlocking {
-                sut.postUserImage(image)
-            }
-        }
-
-        // then
-        val errorBody = exception.response()?.errorBody()?.string()
-        assertNotNull(errorBody)
-
-        val errorResponse = Json.decodeFromString<CommonResponse<String>>(errorBody!!)
-        assertEquals(400, errorResponse.status)
-        assertEquals("프로필 이미지는 최대 5MB까지 업로드할 수 있습니다.", errorResponse.message)
-        assertEquals("M12", errorResponse.code)
-
-        assertNull(errorResponse.data)
     }
 
 //    @Test
