0b21fe871c2aa2a3e844a91ac928448d73f6e549
이건희 <leech9876@naver.com>
2025-07-22 15:52:32 +0900

    #303 - refactor : Flow<Data> 반환에서 Result<Data>로 반환
    먼저 Repository부터 변경하고 테스트코드 만들고 작업해보자

--- core/data/src/main/java/com/youthtalk/repository/LoginRepositoryImpl.kt
+++ core/data/src/main/java/com/youthtalk/repository/LoginRepositoryImpl.kt
@@ -4,40 +4,30 @@ import com.core.dataapi.repository.LoginRepository
 import com.core.datastore.datasource.DataStoreDataSource
 import com.core.exception.NoDataException
 import com.youthtalk.data.LoginService
-import com.youthtalk.dto.MemberId
 import com.youthtalk.dto.login.LoginRequest
 import com.youthtalk.dto.login.SignRequest
 import com.youthtalk.model.typeenum.toRegion
-import com.youthtalk.utils.ErrorUtils.throwableError
+import com.youthtalk.utils.ErrorUtils.createResult
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import timber.log.Timber
 
 class LoginRepositoryImpl @Inject constructor(
     private val loginService: LoginService,
     private val dataStoreDataSource: DataStoreDataSource
 ) : LoginRepository {
-    override fun postLogin(socialId: String): Flow<Long> = flow {
-        Timber.e("LoginRepositoryImpl postLogin start")
-        runCatching { loginService.postLogin(LoginRequest(socialType = "kakao", socialId = socialId).toRequestBody()) }
-            .onSuccess { token ->
-                Timber.e("LoginRepositoryImpl postLogin Success $token")
-                token.data?.let { data ->
-                    emit(data.memberId)
-                } ?: throw NoDataException()
-            }
-            .onFailure { error ->
-                Timber.e("LoginRepositoryImpl postLogin  error : $error")
-                throwableError<MemberId>(error)
-            }
+    override suspend fun postLogin(socialId: String): Result<Long> {
+        return createResult {
+            loginService.postLogin(LoginRequest(socialType = "kakao", socialId = socialId).toRequestBody()).data?.memberId ?: throw NoDataException()
+        }.onFailure { error ->
+            Timber.e("error : $error")
+        }
     }
 
     override fun hasToken(): Flow<Boolean> = dataStoreDataSource.hasToken()
 
-    override fun postSign(id: String, nickname: String, region: String): Flow<Int> = flow {
-        Timber.e("LoginRepositoryImpl start")
-        runCatching {
+    override suspend fun postSign(id: String, nickname: String, region: String): Result<Int> {
+        return createResult {
             loginService.postSignUp(
                 SignRequest(
                     socialType = "kakao",
@@ -45,17 +35,9 @@ class LoginRepositoryImpl @Inject constructor(
                     nickname = nickname,
                     region = region.toRegion().region
                 ).toRequestBody()
-            )
+            ).data ?: throw NoDataException()
+        }.onFailure { error ->
+            Timber.e("error : $error")
         }
-            .onSuccess { response ->
-                Timber.e("LoginRepositoryImpl Success $response")
-                response.data?.let {
-                    emit(it)
-                } ?: throw NoDataException()
-            }
-            .onFailure { error ->
-                Timber.e("LoginRepositoryImpl error : $error")
-                throwableError<Int>(error)
-            }
     }
 }
