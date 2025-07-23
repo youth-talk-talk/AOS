73a42a9eaf8caeb4044801d046058bf59e5d74cc
이건희 <leech9876@naver.com>
2025-07-23 21:57:41 +0900

    #305 - test : 실패 테스트 작성

--- core/data/src/test/java/com/youthtalk/repository/ReportRepositoryTest.kt
+++ core/data/src/test/java/com/youthtalk/repository/ReportRepositoryTest.kt
@@ -1,8 +1,13 @@
 package com.youthtalk.repository
 
+import com.core.exception.BadRequestException
+import com.core.exception.ConflictException
 import com.youthtalk.data.ReportService
 import com.youthtalk.dto.CommonResponse
+import com.youthtalk.dto.toResponseBody
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,6 +16,8 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import retrofit2.HttpException
+import retrofit2.Response
 
 @RunWith(MockitoJUnitRunner::class)
 class ReportRepositoryTest {
@@ -52,6 +59,87 @@ class ReportRepositoryTest {
             // then
             assertEquals(Unit, result)
             verify(reportService).reportComments(commentId)
+        }
+    }
+
+    @Test
+    fun givenAlreadyReportPostId_whenReportPost_thenThrows409Exception() {
+        runTest {
+            // given
+            val reportedPostId = 231L
+
+            whenever(reportService.reportPosts(reportedPostId)).thenThrow(
+                HttpException(
+                    Response.error<Any>(
+                        409,
+                        toResponseBody(CommonResponse<Unit?>(409, "이미 신고한 게시글(또는 댓글)입니다.", "R01", null))
+                    )
+                )
+            )
+
+            // when
+            Assert.assertThrows(ConflictException::class.java) {
+                runBlocking {
+                    sut.reportPost(reportedPostId).getOrThrow()
+                }
+            }
+
+            // then
+            verify(reportService).reportPosts(reportedPostId)
+        }
+    }
+
+    @Test
+    fun givenMyPostId_whenReportPost_thenThrows400Exception() {
+        runTest {
+            // given
+            val myPostId = 123L
+
+            whenever(reportService.reportPosts(myPostId)).thenThrow(
+                HttpException(
+                    Response.error<Any>(
+                        400,
+                        toResponseBody(CommonResponse<Unit?>(400, "본인의 게시글(또는 댓글)은 신고할 수 없습니다.", "R02", null))
+                    )
+                )
+            )
+
+            // when
+            Assert.assertThrows(BadRequestException::class.java) {
+                runBlocking {
+                    sut.reportPost(myPostId).getOrThrow()
+                }
+            }
+
+            // then
+            verify(reportService).reportPosts(myPostId)
+        }
+    }
+
+    @Test
+    fun givenNotFoundPostId_whenReportPost_thenThrows400Exception() {
+        runTest {
+            // given
+            val notFoundPostId = 123123L
+
+            whenever(reportService.reportPosts(notFoundPostId)).thenThrow(
+                HttpException(
+                    Response.error<Any>(
+                        400,
+                        toResponseBody(CommonResponse<Unit?>(400, "해당 게시글을 찾을 수 없습니다.", "PS01", null))
+                    )
+                )
+            )
+
+            // when
+            Assert.assertThrows(BadRequestException::class.java) {
+                runBlocking {
+                    sut.reportPost(notFoundPostId).getOrThrow()
+                }
+            }
+
+            // then
+            verify(reportService).reportPosts(notFoundPostId)
         }
     }
 }
