bf31c3f4dcad738f65601320b02e40de3662555c
이건희 <leech9876@naver.com>
2025-07-25 15:24:09 +0900

    #307 - style : 생성자 간소화

--- core/data/src/test/java/com/youthtalk/repository/model/MessageResponse.kt
+++ core/data/src/test/java/com/youthtalk/repository/model/MessageResponse.kt
@@ -3,6 +3,4 @@ package com.youthtalk.repository.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MessageResponse(
-    val message: List<String>
-)
+data class MessageResponse(val message: List<String>)
