c89927460b9a40e9010ce613f848b9ffabfb3e4d
이건희 <leech9876@naver.com>
2025-11-24 18:53:04 +0900

    [#335] build : 버전 2.3.0으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.youth.yongproject.app"
         targetSdk = 35
-        versionCode = 20200
-        versionName = "2.2.0"
+        versionCode = 20251124
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
